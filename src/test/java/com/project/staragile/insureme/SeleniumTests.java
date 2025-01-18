c0bfe7cac20e95bf3cb54d2388be03ef2e517133
Undertaker69cyprus <139839403+Undertaker69cyprus@users.noreply.github.com>
2025-01-18 22:20:30 +0530

    Update SeleniumTests.java

--- src/test/java/com/project/staragile/insureme/SeleniumTests.java
+++ src/test/java/com/project/staragile/insureme/SeleniumTests.java
@@ -2,16 +2,16 @@ package com.project.staragile.insureme;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SeleniumTests {
+    private static WebDriver driver;
 
-    private WebDriver driver;
-
-    @BeforeClass
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         // Set up WebDriver (ChromeDriver as an example)
         System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
         driver = new ChromeDriver();
@@ -21,11 +21,11 @@ public class SeleniumTests {
     public void testHomePageTitle() {
         driver.get("http://18.212.69.204:8084"); // Replace with your app URL
         String pageTitle = driver.getTitle();
-        assert pageTitle.contains("Test"); // Replace "Expected Title" accordingly
+        assertTrue(pageTitle.contains("Test"), "Page title should contain 'Test'"); 
     }
 
-    @AfterClass
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown() {
         if (driver != null) {
             driver.quit();
         }
