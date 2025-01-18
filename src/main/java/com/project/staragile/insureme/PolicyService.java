024189c6776a616f38016f4fd56a9a147180815b
Undertaker69cyprus <139839403+Undertaker69cyprus@users.noreply.github.com>
2025-01-18 19:54:23 +0530

    Update PolicyService.java

--- src/main/java/com/project/staragile/insureme/PolicyService.java
+++ src/main/java/com/project/staragile/insureme/PolicyService.java
@@ -3,51 +3,42 @@ package com.project.staragile.insureme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class PolicyService {
-	
-	@Autowired
-	PolicyRepository policyRepository;
-	
-	
-	public Policy CreatePolicy() {
-		
-		Policy policy = generateDummyPolicy();
-		
-		return policyRepository.save(policy);
-		
-			
-	}
-	
-	public Policy updatePolicy() {
-		
-		return null;
-	}
-	
-	public Policy deletePolicy() {
-		
-		return null;
-	}
-	
-	public Policy searchPolicy() {
-		
-		return null;
-	}
-	
-	
-	public Policy generateDummyPolicy() {
-		return new Policy(1, "Shubham", "Individual" , 10000, "10-Sep-2021", "10-Sep-2022");
-	}
 
-	public Policy registerPolicy(Policy policy) {
-		// TODO Auto-generated method stub
-		return policyRepository.save(policy);
-	}
+    @Autowired
+    PolicyRepository policyRepository;
+    
+    public Policy createPolicy() {
+        Policy policy = generateDummyPolicy();
+        return policyRepository.save(policy);  // Ensure it returns the saved Policy object
+    }
 
-	public Policy getPolicyDetails(int policyId) {
-		// TODO Auto-generated method stub
-		return policyRepository.findById(policyId).get();
-	}
+    public Policy updatePolicy(Policy updatedPolicy) {
+        return policyRepository.save(updatedPolicy);  // Save the updated Policy object
+    }
+    
+    public boolean deletePolicy(Long policyId) {
+        if (policyRepository.existsById(policyId)) {
+            policyRepository.deleteById(policyId);
+            return true;
+        }
+        return false;
+    }
+    
+    public Policy searchPolicy(Long policyId) {
+        return policyRepository.findById(policyId).orElse(null);  // Return Policy if found, otherwise null
+    }
 
+    public Policy generateDummyPolicy() {
+        return new Policy(1, "Shubham", "Individual", 10000, "10-Sep-2021", "10-Sep-2022");
+    }
+
+    public Policy registerPolicy(Policy policy) {
+        return policyRepository.save(policy);  // Save the Policy and return it
+    }
+
+    public Policy getPolicyDetails(Long policyId) {
+        return policyRepository.findById(policyId).orElse(null);  // Return Policy details if found, otherwise null
+    }
 }
