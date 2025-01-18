7dec9a08f38ac75409e1f610c2729a56aff9b971
Undertaker69cyprus <139839403+Undertaker69cyprus@users.noreply.github.com>
2025-01-18 18:09:29 +0530

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO policy (name, type, details) VALUES ('Health Insurance', 'Health', 'Covers medical expenses');
+INSERT INTO policy (name, type, details) VALUES ('Life Insurance', 'Life', 'Provides life coverage');
