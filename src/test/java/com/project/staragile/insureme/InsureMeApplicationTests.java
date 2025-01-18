acaedf98d11664372b051782e2031b6ba8b48387
Undertaker69cyprus <139839403+Undertaker69cyprus@users.noreply.github.com>
2025-01-18 18:11:57 +0530

    Update InsureMeApplicationTests.java

--- src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
+++ src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
@@ -1,31 +1,35 @@
 package com.project.staragile.insureme;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InsureMeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-	
-	@Test
-	void testCreatePolicy() {
-		Policy policy = new Policy(1, "Shubham", "Individual" , 10000, "10-Sep-2021", "10-Sep-2022");
-		PolicyService pService = new PolicyService();
-		//Policy outputPolicy = pService.CreatePolicy();
-		assertEquals(policy.getPolicyId(), pService.generateDummyPolicy().getPolicyId());
-		
-	}
-	
-	@Test
-	void testSearchPolicy() {
-		PolicyService pService = new PolicyService();
-		assertEquals(null,pService.searchPolicy());
-	}
+    @Autowired
+    private PolicyService policyService;
 
-	
+    @Test
+    void contextLoads() {
+        assertNotNull(policyService, "PolicyService should be auto-wired and not null");
+    }
+
+    @Test
+    void testCreatePolicy() {
+        Policy policy = new Policy(null, "Shubham", "Individual", 10000, "10-Sep-2021", "10-Sep-2022");
+        Policy createdPolicy = policyService.createPolicy(policy);
+        assertNotNull(createdPolicy.getPolicyId(), "Policy ID should be auto-generated and not null");
+        assertEquals("Shubham", createdPolicy.getPolicyHolderName());
+    }
+
+    @Test
+    void testSearchPolicy() {
+        Long nonExistentPolicyId = 999L;
+        Policy policy = policyService.viewPolicy(nonExistentPolicyId);
+        assertEquals(null, policy, "Search should return null for a non-existent policy");
+    }
 }
