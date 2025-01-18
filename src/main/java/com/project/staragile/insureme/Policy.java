7c532c2ef543a4ade232f1c8e42fe48cb00b984e
Undertaker69cyprus <139839403+Undertaker69cyprus@users.noreply.github.com>
2025-01-18 21:50:19 +0530

    Update Policy.java

--- src/main/java/com/project/staragile/insureme/Policy.java
+++ src/main/java/com/project/staragile/insureme/Policy.java
@@ -1,6 +1,5 @@
 package com.project.staragile.insureme;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,87 +7,78 @@ import javax.persistence.Id;
 
 @Entity
 public class Policy {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int policyId;
-	
-	private String policyHolderName;
-	
-	private String policyType;
-	
-	private double policyPrice;
-	
-	private String policyStartDate;
-	
-	private String policyEndDate;
-	
-	
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int policyId;
+    
+    private String policyHolderName;
+    private String policyType;
+    private double policyPrice;
+    private String policyStartDate;
+    private String policyEndDate;
+    
+    public Policy() {
+        super();
+    }
 
-	public Policy() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public Policy(int policyId, String policyHolderName, String policyType, double policyPrice, String policyStartDate,
+            String policyEndDate) {
+        super();
+        this.policyId = policyId;
+        this.policyHolderName = policyHolderName;
+        this.policyType = policyType;
+        this.policyPrice = policyPrice;
+        this.policyStartDate = policyStartDate;
+        this.policyEndDate = policyEndDate;
+    }
 
-	public Policy(int policyId, String policyHolderName, String policyType, double policyPrice, String policyStartDate,
-			String policyEndDate) {
-		super();
-		this.policyId = policyId;
-		this.policyHolderName = policyHolderName;
-		this.policyType = policyType;
-		this.policyPrice = policyPrice;
-		this.policyStartDate = policyStartDate;
-		this.policyEndDate = policyEndDate;
-	}
+    // Getters and Setters remain the same
+    public int getPolicyId() {
+        return policyId;
+    }
 
-	public int getPolicyId() {
-		return policyId;
-	}
+    public void setPolicyId(int policyId) {
+        this.policyId = policyId;
+    }
 
-	public void setPolicyId(int policyId) {
-		this.policyId = policyId;
-	}
+    public String getPolicyHolderName() {
+        return policyHolderName;
+    }
 
-	public String getPolicyHolderName() {
-		return policyHolderName;
-	}
+    public void setPolicyHolderName(String policyHolderName) {
+        this.policyHolderName = policyHolderName;
+    }
 
-	public void setPolicyHolderName(String policyHolderName) {
-		this.policyHolderName = policyHolderName;
-	}
+    public String getPolicyType() {
+        return policyType;
+    }
 
-	public String getPolicyType() {
-		return policyType;
-	}
+    public void setPolicyType(String policyType) {
+        this.policyType = policyType;
+    }
 
-	public void setPolicyType(String policyType) {
-		this.policyType = policyType;
-	}
+    public double getPolicyPrice() {
+        return policyPrice;
+    }
 
-	public double getPolicyPrice() {
-		return policyPrice;
-	}
+    public void setPolicyPrice(double policyPrice) {
+        this.policyPrice = policyPrice;
+    }
 
-	public void setPolicyPrice(double policyPrice) {
-		this.policyPrice = policyPrice;
-	}
+    public String getPolicyStartDate() {
+        return policyStartDate;
+    }
 
-	public String getPolicyStartDate() {
-		return policyStartDate;
-	}
+    public void setPolicyStartDate(String policyStartDate) {
+        this.policyStartDate = policyStartDate;
+    }
 
-	public void setPolicyStartDate(String policyStartDate) {
-		this.policyStartDate = policyStartDate;
-	}
+    public String getPolicyEndDate() {
+        return policyEndDate;
+    }
 
-	public String getPolicyEndDate() {
-		return policyEndDate;
-	}
-
-	public void setPolicyEndDate(String policyEndDate) {
-		this.policyEndDate = policyEndDate;
-	}
-	
-	
-
+    public void setPolicyEndDate(String policyEndDate) {
+        this.policyEndDate = policyEndDate;
+    }
 }
